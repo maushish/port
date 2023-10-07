804b28faaf1ed9e5f20b896dbb3bee06eaef833d
maushish <chamarnumber102@gmail.com>
2023-10-07 09:45:27 +0530

    Adding small changes in the main page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        'user-bold':['User-Bold'],
+        'user':['User'],
+
+      },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
       backgroundImage:{
         'main':"url('/noise.png')",
         'graph':"url('/graphs.png')",
@@ -24,7 +42,8 @@ module.exports = {
       fontSize:{
         'lamba':'11rem',
         'lund':'14rem',
-      }
+      },
+
     },
   },
   plugins: [],
