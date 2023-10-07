3c906ba2a07823e54a1d79495c1a1867d5aee791
maushish <chamarnumber102@gmail.com>
2023-10-07 13:25:04 +0530

    Component introduced

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,8 +11,9 @@ export default function navbar(){
           <div className='bg-graph absolute top-[0.5vh] left-[20vh] w-[120vh] h-[44vh] mix-blend-overlay'>
           </div>
         </div>
-        <h1 className='text-white text-lamba absolute left-[73vh] top-[15vh] font-bold '>THIS IS</h1>
-        <h1 className='text-white  text-lund absolute left-[48vh] top-[35vh] '><span className=' text-transparent bg-clip-text bg-gradient-to-b from-emerald-500 via-emerald-500 to-cyan-500 bg-300% animation-gradient font-bold'>MAUSHISH</span></h1>
+        <div className=" font-lomo">
+        <h1 className='text-white text-lamba absolute left-[73vh] top-[15vh]'>THIS IS</h1>
+        <h1 className='text-white  text-lund absolute left-[48vh] top-[35vh] '><span className=' text-transparent bg-clip-text bg-gradient-to-b from-emerald-500 via-emerald-500 to-cyan-500 bg-300% animation-gradient'>MAUSHISH</span></h1>
         <p className=' text-gray-400 text-5xl absolute top-[65vh] left-[55vh] font-user-bold'>A 19 Yr Old boi who just wanna code!</p>
         <a>
           <button type='button' className='px-20  bg-black py-4 border-[6px] text-4xl  rounded-full absolute right-[60vh] top-[80vh] text-white font-bold'>RESUME</button>
@@ -20,6 +21,7 @@ export default function navbar(){
         <a>
           <button type='button' className='px-14 py-4  bg-gradient-to-bl from-emerald-500 via-emerald-500 to-cyan-500 border-[6px] text-4xl rounded-full absolute left-[70vh] top-[80vh] text-white font-bold'>LET'S WORK</button>
         </a>
+        </div>
         
       </header>
     )
