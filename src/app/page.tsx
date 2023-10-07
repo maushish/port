a49d60b0d05f481df8387b0ff5286ccb6811a30a
maushish <chamarnumber102@gmail.com>
2023-10-07 10:22:53 +0530

    Warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,13 @@ export default function Home() {
       <h1 className='text-white  text-lund absolute left-[48vh] top-[35vh] '><span className=' text-transparent bg-clip-text bg-gradient-to-b from-emerald-500 via-emerald-500 to-cyan-500 bg-300% animation-gradient font-bold'>MAUSHISH</span></h1>
       <p className=' text-gray-400 text-5xl absolute top-[65vh] left-[55vh] font-user-bold'>A 19 Yr Old boi who just wanna code!</p>
       <a>
-        <button className='px-20  bg-black py-4 border-[6px] text-4xl  rounded-full absolute right-[60vh] top-[80vh] text-white font-bold'>RESUME</button>
+        <button type='button' className='px-20  bg-black py-4 border-[6px] text-4xl  rounded-full absolute right-[60vh] top-[80vh] text-white font-bold'>RESUME</button>
       </a>
       <a>
-        <button className='px-14 py-4  bg-gradient-to-bl from-emerald-500 via-emerald-500 to-cyan-500 border-[6px] text-4xl rounded-full absolute left-[70vh] top-[80vh] text-white font-bold'>LET'S WORK</button>
+        <button type='button' className='px-14 py-4  bg-gradient-to-bl from-emerald-500 via-emerald-500 to-cyan-500 border-[6px] text-4xl rounded-full absolute left-[70vh] top-[80vh] text-white font-bold'>LET'S WORK</button>
       </a>
       
     </header>
+
   )
 }
